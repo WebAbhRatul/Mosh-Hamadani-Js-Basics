979ce611f6663ef233fbd2b4e45e9a82669a78ab
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-18 09:29:49 +0600

    type of operator

--- primitive-var.js
+++ primitive-var.js
@@ -3,3 +3,7 @@ let age = 26; //Number litarels
 let student = true; //Boolean literals
 let id; // undefined literals
 let books = null; // null literals
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof id);
+console.log(typeof books);
