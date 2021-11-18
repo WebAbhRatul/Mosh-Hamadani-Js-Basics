e6939c4c5cb39952b7af595fa4998dc7c8a601b2
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-18 16:36:36 +0600

    Arithmetic & comparison operator

---
+++ arithmetic-op.js
@@ -0,0 +1,39 @@
+// Arithmetic operator 
+console.log('Arithmetic operator');
+let x = 10;
+let y = 5;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+
+// increment operator 
+console.log(x++);
+console.log(x);
+console.log(x);
+
+
+// decrement operator
+console.log(x--);
+console.log(x);
+
+
+// Assignment operator 
+console.log('Assignment operator')
+
+x += 77;
+console.log(x);
+
+x *= 10;
+console.log(x);
+
+
+
+
+
+
+
+
---
+++ comparison.js
@@ -0,0 +1,12 @@
+let x = 1;
+
+// relational operator 
+console.log(x > 1);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+// equality operator 
+console.log('Equality operator: ');
+console.log(x === 1);
+console.log(x !== 1);
