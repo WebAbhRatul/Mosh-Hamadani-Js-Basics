b604699e3b1f368b181d38310b9fcc5dab4d4d6a
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-18 11:30:25 +0600

    Function

---
+++ function.js
@@ -0,0 +1,13 @@
+
+// performing a task 
+function greet(firstName, lastName) {  //paramater
+    console.log('Hello '+ firstName +' '+lastName);
+}
+greet('Abh', 'Ratul');//Argumant
+greet('Abu','Hurayra');
+
+// calculating a value 
+function square(number){
+    return number * number ;
+}
+console.log(square(4));
