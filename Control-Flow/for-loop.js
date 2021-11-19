096dd725a128b68db869d8e77c1f3dbed2683320
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-19 08:17:48 +0600

    Control Flow

---
+++ Control-Flow/for-loop.js
@@ -0,0 +1,12 @@
+for(let i=0; i<5; i++){
+    console.log('Hello world ',i);
+}
+
+for(i=5; i>0; i--){
+    console.log('Hello world ',i);
+}
+console.log('Even and odd number ');
+for(i=0; i<=20; i++){
+    if(i%2 !== 0) console.log(i, ' Odd number');
+    else console.log(i, 'Even number');
+}
