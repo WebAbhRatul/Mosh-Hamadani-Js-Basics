7a3411569f704704c6397d45853f5edbe6be44b8
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-22 08:06:46 +0600

    Object

--- object/factory-function.js
+++ object/factory-function.js
@@ -9,3 +9,31 @@ function createCircle(radius){
 let circle1 = createCircle(5,7,9);
 console.log(circle1.draw(3,7));
 console.log(circle1.radius);
+
+const name = {
+    name:'Ratul',
+    age: 20
+}
+console.log(name['name']);
+console.log(name['age']);
+
+// JavaScript Nested Objects
+const student={
+    name: 'Maggy',
+    age: 26,
+    class: 10,
+    marks:{
+        math:40,
+        bangla:{
+            firstPaper:50,
+            secondPaper:70
+        },
+        english:90
+    },
+    grade:"A+"
+
+   
+}
+console.log(student.marks.bangla.secondPaper);
+console.log(student.marks.bangla.firstPaper);
+console.log(student.grade);
