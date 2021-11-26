ac5158ea8b6a185ccbc200c6ff5337e00528a4a6
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-26 08:40:22 +0600

    Array

---
+++ array/finding-elements-primitive.js
@@ -0,0 +1,7 @@
+const alphaBet = ['a', 'b', 'c', 'd', 'e', 'f', 'a' ,'g','a'];
+
+console.log(alphaBet.indexOf('a',3));
+console.log(alphaBet.indexOf(-1));
+console.log(alphaBet.lastIndexOf('a'));
+console.log(alphaBet.indexOf('a') !== -1);
+console.log(alphaBet.includes(1));
