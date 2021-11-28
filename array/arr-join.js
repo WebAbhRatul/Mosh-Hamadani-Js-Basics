60277ed195b1bd75001217fd21f3dfc176394911
WebAbhRatul <web.abh.ratul@gmail.com>
2021-11-28 08:21:02 +0600

    Array

---
+++ array/arr-join.js
@@ -0,0 +1,15 @@
+const numbers = [1,2,3,4];
+const joined = numbers.join(',');
+console.log(joined);
+console.log(typeof joined);
+
+const str = "This is my new Home";
+let stt = str.split(' ');
+console.log(stt);
+console.log(typeof stt);
+console.log(stt.join('-'), typeof stt.join('-'));
+
+
+
+
+
---
+++ array/iterating-arr.js
@@ -0,0 +1,7 @@
+let num = [1,2,3,4];
+// for(let item of num) 
+//     console.log(item);
+
+num.forEach(function(number,index){
+    console.log(index, number);
+});
