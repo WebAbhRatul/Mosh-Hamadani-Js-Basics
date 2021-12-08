b43d75efb62632d7859efa01a2b6122be130e778
WebAbhRatul <web.abh.ratul@gmail.com>
2021-12-08 20:39:44 +0600

    Function

--- Function/getters-setters.js
+++ Function/getters-setters.js
@@ -5,12 +5,21 @@ const person = {
         console.log(`${person.firstName} ${person.lastName}`);
     },
     set fullname(value){
+        if(typeof value !== 'string')
+            throw new Error('This is not a string');
        let parts =  value.split(' ');
+       if(parts.length !== 2)
+            throw new Error('Enter first and last name');
        this.firstName = parts[0];
        this.lastName = parts[1];
     }
+};
+
+try{
+    person.fullname = '';
+}
+catch(e){
+    console.log(e);
 }
 
-person.fullname = 'Abh Hurayra';
-console.log(person.lastName);
 person.fullname;
