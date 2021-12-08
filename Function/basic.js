8e6245bfefeced82237f5b2510b991bc0bcc28f1
WebAbhRatul <web.abh.ratul@gmail.com>
2021-12-08 06:53:39 +0600

    Function

---
+++ Function/basic.js
@@ -0,0 +1,21 @@
+// Function Declaration 
+function student(){
+    console.log('My name is Ratul');
+}
+student();
+
+//Function Expression 
+let profile1 = function student1(){
+    console.log('My name is Perpendicular');
+}
+
+profile1();
+
+let profile2 = profile1;
+profile2();
+
+//Anonymous Function Expression
+let profile3 = function(){
+    console.log('My name is Realme');
+}
+profile3();
